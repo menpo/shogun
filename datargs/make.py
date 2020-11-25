bdd6645947ea3211e0849411b055abbe7ac77e19
Patrick Snape <patricksnape@gmail.com>
2020-11-25 15:40:26 +0100

    Initial messy implementation of allowing nested dataclasses
    Modified how the type dispatching works to properly recurse
    on dataclasses and automatically generate nested parameters
    of the form:
    
    ```
    @argsclass(description="Nested")
    class Nested:
        some_value: int = arg(help="Some Nested value arg", default=3)
    
    @argsclass(description="OptimizationParams")
    class OptimizationParams:
        learning_rate: float = 0.1
        nested_arg: Nested = arg(help="Nested arg", default_factory=Nested)
    ```
    
    Generates the following --help
    
    ```
    usage: scratch_2.py [-h] [--learning-rate LEARNING_RATE]
                        [--nested-arg-some-value NESTED_ARG_SOME_VALUE]
    
    OptimizationParams
    
    optional arguments:
      -h, --help            show this help message and exit
      --learning-rate LEARNING_RATE
      --nested-arg-some-value NESTED_ARG_SOME_VALUE
                            Some Nested value arg (default: 3)
    ```

--- datargs/make.py
+++ datargs/make.py
@@ -43,151 +43,215 @@ SystemExit: 2
 
 Specifying enums by name is not currently supported.
 """
-# noinspection PyUnresolvedReferences,PyProtectedMember
-from argparse import (
-    ArgumentParser,
-    ArgumentTypeError,
-    _SubParsersAction,
-    Namespace,
-)
-from enum import Enum
-from functools import wraps, partial
-from inspect import signature
-from typing import (
-    Callable,
-    Dict,
-    TypeVar,
-    Type,
-    Sequence,
-    Optional,
-    overload,
-    Any,
-    Mapping,
-    Union,
-    cast,
-    get_type_hints,
-)
 
 import dataclasses
-from dataclasses import dataclass, MISSING
 
-from .compat import RecordField, RecordClass, NotARecordClass, DatargsParams
+# noinspection PyUnresolvedReferences,PyProtectedMember
+import inspect
+from abc import ABCMeta, abstractmethod
+from argparse import (
+    ArgumentDefaultsHelpFormatter,
+    ArgumentParser,
+    ArgumentTypeError,
+)
+from collections import deque
+from enum import Enum
+from functools import partial
+from typing import (
+    Any,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Sequence,
+    Type,
+    TypeVar,
+    get_type_hints,
+    overload,
+)
+
+from .compat import DatargsParams, NotARecordClass, RecordClass, RecordField
 
 
-@dataclass
+@dataclasses.dataclass()
 class Action:
-    args: Sequence[Any] = dataclasses.field(default_factory=list)
+    aliases: Sequence[str] = dataclasses.field(default_factory=list)
     kwargs: Mapping[str, Any] = dataclasses.field(default_factory=dict)
 
 
-DispatchCallback = Callable[[str, RecordField], Action]
-AddArgFunc = Callable[[RecordField], Action]
-
-
-def field_name_to_arg_name(name: str, positional=False) -> str:
-    if positional:
-        return name
-    return f"--{name.replace('_','-')}"
+def field_name_to_arg_name(name: str, underscore_to_hyphen: bool = True) -> str:
+    return f"--{name.replace('_', '-') if underscore_to_hyphen else name}"
 
 
 class TypeDispatch:
-
-    dispatch: Dict[type, AddArgFunc] = {}
-
-    @classmethod
-    def add_arg(cls, field: RecordField):
-        for typ, func in cls.dispatch.items():
-            if issubclass(field.type, typ):
-                return func(field)
-        return add_any(field)
+    # Intentionally static
+    dispatch: List["DispatcherBase"] = []
 
     @classmethod
-    def register(cls, typ):
-        def decorator(func: DispatchCallback) -> AddArgFunc:
-            cls.dispatch[typ] = new_func = add_name_formatting(func)
-            return new_func
+    def build_actions(cls, field: RecordField) -> Sequence[Action]:
+        for dispatcher in cls.dispatch:
+            if dispatcher.is_type(field.type):
+                return dispatcher.build_actions(field)
+        return (add_default(field),)
 
-        return decorator
-
-
-def add_name_formatting(func: DispatchCallback) -> AddArgFunc:
-    @wraps(func)
-    def new_func(field: RecordField):
-        return func(
-            field_name_to_arg_name(field.name, positional=field.is_positional),
-            field,
-        )
-
-    return new_func
+    @classmethod
+    def register(cls, dispatcher_type: Type["DispatcherBase"]):
+        cls.dispatch.append(dispatcher_type())
 
 
-@add_name_formatting
-def add_any(name: str, field: RecordField) -> Action:
-    return add_default(name, field)
+class DispatcherBase(metaclass=ABCMeta):
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if not inspect.isabstract(cls):
+            TypeDispatch.register(cls)
+
+    @abstractmethod
+    def is_type(self, field_type: type) -> bool:
+        ...
+
+    @abstractmethod
+    def build_actions(self, field: RecordField) -> Sequence[Action]:
+        ...
 
 
-def get_option_strings(name: str, field: RecordField):
+class DispatcherIsSubclass(DispatcherBase):
+    type_: type
+
+    @abstractmethod
+    def build_action(self, field: RecordField) -> Action:
+        ...
+
+    def is_type(self, field_type: type) -> bool:
+        return issubclass(field_type, self.type_)
+
+    def build_actions(self, field: RecordField) -> Sequence[Action]:
+        return (self.build_action(field),)
+
+
+def get_arg_alias_list(name: str, field: RecordField) -> List[str]:
     return [name, *field.metadata.get("aliases", [])]
 
 
 def common_kwargs(field: RecordField):
-    return {"type": field.type, **subdict(field.metadata, {"aliases"})}
+    return {"type": field.type, **filter_dict(field.metadata, {"aliases"})}
 
 
-def subdict(dct, remove_keys):
+def filter_dict(dct, remove_keys):
     return {key: value for key, value in dct.items() if key not in remove_keys}
 
 
-def add_default(name, field: RecordField, **kwargs) -> Action:
+def add_default(field: RecordField, **kwargs) -> Action:
     kwargs = {
         "default": field.default,
-        "field": field.is_required(),
+        "required": field.is_required(),
         **common_kwargs(field),
         **kwargs,
     }
-    return Action(kwargs=kwargs, args=get_option_strings(name, field))
+    return Action(
+        aliases=get_arg_alias_list(field_name_to_arg_name(field.name), field),
+        kwargs=kwargs,
+    )
 
 
 T = TypeVar("T")
 
 
-def call_func_with_matching_kwargs(func: Callable[..., T], *args, **kwargs) -> T:
-    sig = signature(func)
-    new_kwargs = {key: value for key, value in kwargs.items() if key in sig.parameters}
-    return func(*args, **new_kwargs)
+class DispatcherIsDataclass(DispatcherBase):
+    def is_type(self, field_type: type) -> bool:
+        return dataclasses.is_dataclass(field_type)
+
+    def _add_prefix(self, prefix: str, aliases: Sequence[str]) -> Sequence[str]:
+        new_aliases = []
+        for alias in aliases:
+            assert alias[:2] == "--"
+            new_alias = f"--{prefix}-{alias[2:]}"
+            new_aliases.append(new_alias)
+        return new_aliases
+
+    def build_actions(self, field: RecordField) -> Sequence[Action]:
+        # TODO: Make this configurable
+        prefix = field.name.replace("_", "-")
+
+        actions = []
+        record_class = RecordClass.wrap_class(field.type)
+        for sub_field in record_class.fields_dict().values():
+            sub_actions = TypeDispatch.build_actions(sub_field)
+
+            for s_a in sub_actions:
+                s_a.aliases = self._add_prefix(prefix, s_a.aliases)
+
+            actions.extend(sub_actions)
+
+        return actions
 
 
-@TypeDispatch.register(bool)
-def bool_arg(name: str, field: RecordField) -> Action:
-    kwargs = {
-        **subdict(common_kwargs(field), ["type"]),
-        "action": "store_false"
-        if field.default and field.has_default()
-        else "store_true",
-    }
-    return Action(
-        args=get_option_strings(name, field),
-        kwargs=kwargs,
-    )
+class DispatcherBool(DispatcherIsSubclass):
+    type_ = bool
+
+    def build_action(self, field: RecordField) -> Action:
+        kwargs = {
+            **filter_dict(common_kwargs(field), {"type"}),
+            "action": "store_false"
+            if field.default and field.has_default()
+            else "store_true",
+        }
+        return Action(
+            aliases=get_arg_alias_list(
+                field_name_to_arg_name(field.name),
+                field,
+            ),
+            kwargs=kwargs,
+        )
 
 
-@TypeDispatch.register(Enum)
-def enum_arg(name: str, field: RecordField) -> Action:
-    def enum_type_func(value: str):
-        result = field.type.__members__.get(value)
-        if not result:
-            raise ArgumentTypeError(
-                f"invalid choice: {value!r} (choose from {[e.name for e in field.type]})"
-            )
-        return result
+class DispatcherEnum(DispatcherIsSubclass):
+    type_ = Enum
 
-    return add_default(
-        name,
-        field,
-        type=enum_type_func,
-        choices=field.type,
-        metavar=f"{{{','.join(field.type.__members__)}}}",
-    )
+    def build_action(self, field: RecordField) -> Action:
+        def enum_type_func(value: str):
+            result = field.type.__members__.get(value)
+            if not result:
+                raise ArgumentTypeError(
+                    f"invalid choice: {value!r} (choose from {[e.name for e in field.type]})"
+                )
+            return result
+
+        return add_default(
+            field,
+            type=enum_type_func,
+            choices=field.type,
+            metavar=f"{{{','.join(field.type.__members__)}}}",
+        )
+
+
+def build_record_instance_from_parsed_args(
+    record_class: RecordClass, args: Dict[str, Any]
+):
+    # New dictionary of args guaranteed to iterate by sorted key
+    args = {k: v for k, v in sorted(args.items())}
+    unpacked = {}
+    for field_name, field in sorted(record_class.fields_dict().items()):
+        for arg_name in list(args):
+            if arg_name == field_name:
+                unpacked[field_name] = args.pop(arg_name)
+                break
+            elif arg_name.startswith(field_name):
+                slice_index = len(field_name) + 1
+                sub_args = {}
+
+                sub_arg_names = deque(args)
+                while sub_arg_names and sub_arg_names[0].startswith(field_name):
+                    sa_name = sub_arg_names.popleft()
+                    sub_args[sa_name[slice_index:]] = args.pop(sa_name)
+
+                sub_record_class = RecordClass.wrap_class(field.type)
+                sub_value = build_record_instance_from_parsed_args(
+                    sub_record_class, sub_args
+                )
+                unpacked[field_name] = sub_value
+                break
+
+    return record_class.cls(**unpacked)
 
 
 ParserType = TypeVar("ParserType", bound=ArgumentParser)
@@ -236,10 +300,21 @@ def make_parser(cls, parser=None):
 def _make_parser(record_class: RecordClass, parser: ParserType = None) -> ParserType:
     if not parser:
         parser = ArgumentParser(**record_class.parser_params)
-    assert parser is not None
+
     for name, field in record_class.fields_dict().items():
-        action = TypeDispatch.add_arg(field)
-        parser.add_argument(*action.args, **action.kwargs)
+        # TODO: Use the correct description - need to push creating the arguments
+        #       into the dispatcher
+        group = parser
+
+        actions = TypeDispatch.build_actions(field)
+        if len(actions) > 1:
+            group = parser.add_argument_group(
+                description=record_class.datargs_params.parser.get("description")
+            )
+
+        for action in actions:
+            group.add_argument(*action.aliases, **action.kwargs)
+
     return parser
 
 
@@ -260,21 +335,14 @@ def parse(cls: Type[T], args: Optional[Sequence[str]] = None, *, parser=None) ->
     Args(is_flag=False, num=1)
     """
     result = vars(make_parser(cls, parser=parser).parse_args(args))
-    try:
-        command_dest = cls.__datargs_params__.sub_commands.get("dest", None)
-    except AttributeError:
-        pass
-    else:
-        if command_dest is not None and command_dest in result:
-            del result[command_dest]
-    return cls(**result)
+    return build_record_instance_from_parsed_args(RecordClass.wrap_class(cls), result)
 
 
 def argsclass(
     cls: type = None,
     *args,
     description: str = None,
-    parser_params: dict = None,
+    parser_params: Dict[str, Any] = None,
     **kwargs,
 ):
     """
@@ -286,6 +354,8 @@ def argsclass(
         "description": description,
         "parser_params": parser_params,
     }
+    parser_params.setdefault("formatter_class", ArgumentDefaultsHelpFormatter)
+
     if cls is None:
         # We're called with parens.
         return partial(
@@ -314,12 +384,15 @@ def make_class(
         RecordClass.wrap_class(cls)
     except NotARecordClass:
         for key, value in cls.__dict__.items():
-            if not isinstance(value, dataclasses.Field) or value.default is not MISSING:
+            if (
+                not isinstance(value, dataclasses.Field)
+                or value.default is not dataclasses.MISSING
+            ):
                 continue
             typ = value.type or get_type_hints(cls)[key]
             if typ is bool:
                 value.default = False
-        new_cls = dataclass(*args, **kwargs)(cls)
+        new_cls = dataclasses.dataclass(*args, **kwargs)(cls)
     else:
         new_cls = cls
     new_cls.__datargs_params__ = DatargsParams(
@@ -332,7 +405,7 @@ def make_class(
 def arg(
     nargs=None,
     const=None,
-    default=MISSING,
+    default=dataclasses.MISSING,
     choices=None,
     help=None,
     metavar=None,
