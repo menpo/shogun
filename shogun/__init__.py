3fe5bbbb48ea3084a2aa48b6e03b431164cd2f91
Patrick Snape <patricksnape@gmail.com>
2020-11-25 21:27:34 +0100

    Move the last of the files into sub-packages
    And delete the make.py file - move the header about datargs into
    the __init__.py

--- shogun/__init__.py
+++ shogun/__init__.py
@@ -1,3 +1,8 @@
-from .make import parse, arg, make_parser, argsclass
+# Initial implementation courtesy of https://github.com/roee30/datargs
+# under the MIT license - maintained in this repository at LICENSE-datargs
 
-__all__ = ["parse", "arg", "make_parser", "argsclass"]
+from .argparse_.args import arg
+from .argparse_.argsclass import argsclass
+from .argparse_.parse import parse
+
+__all__ = ["arg", "argsclass", "parse"]
