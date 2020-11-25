ccdf713fde8f95c505b38d5b24506d4fd8643281
Patrick Snape <patricksnape@gmail.com>
2020-11-25 21:48:02 +0100

    Add miniver version and setup.py
    Prepping for uploading to pypi

--- shogun/__init__.py
+++ shogun/__init__.py
@@ -1,8 +1,9 @@
 # Initial implementation courtesy of https://github.com/roee30/datargs
 # under the MIT license - maintained in this repository at LICENSE-datargs
 
+from ._version import __version__
 from .argparse_.args import arg
 from .argparse_.argsclass import argsclass
 from .argparse_.parse import parse
 
-__all__ = ["arg", "argsclass", "parse"]
+__all__ = ["arg", "argsclass", "parse", "__version__"]
