0c66f046227bde591b8199c8e05c15beb60c0c99
Patrick Snape <patricksnape@gmail.com>
2020-11-29 20:32:25 +0100

    Add implementation of generic dispatching
    Also add a special Literal dispatcher that works similarly
    to the Enum type

--- shogun/utils.py
+++ shogun/utils.py
@@ -1,9 +1,18 @@
-from typing import Any, Container, Dict
+import sys
+from distutils.version import StrictVersion
+from typing import Any, Container, Dict, Mapping
+
+PY_VER = StrictVersion(f"{sys.version_info.major}.{sys.version_info.minor}")
 
 
-def filter_dict(dct: Dict[str, Any], remove_keys: Container[str]) -> Dict[str, Any]:
+IS_PYTHON_36 = PY_VER == StrictVersion("3.6")
+IS_PYTHON_37 = PY_VER == StrictVersion("3.7")
+IS_GT_PYTHON_38 = PY_VER >= StrictVersion("3.8")
+
+
+def filter_dict(dct: Mapping[str, Any], remove_keys: Container[str]) -> Dict[str, Any]:
     return {key: value for key, value in dct.items() if key not in remove_keys}
 
 
-def remove_dict_nones(dct: Dict[str, Any]) -> Dict[str, Any]:
+def remove_dict_nones(dct: Mapping[str, Any]) -> Dict[str, Any]:
     return {key: value for key, value in dct.items() if value is not None}
